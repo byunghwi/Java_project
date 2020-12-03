d2b92e5105adee7a7d906a6ad5833d1d86ceb66f
gpdld <gpdld@175.115.175.207>
2020-12-03 16:08:44 +0900

    hikari test by byunghwi

--- src/db/DatabaseConnect.java
+++ src/db/DatabaseConnect.java
@@ -14,6 +14,7 @@ public class DatabaseConnect {
 	static HikariDataSource ds;
 	static Connection conn;
 	static PreparedStatement pstmt;
+	static ResultSet rs;
 	
 	static {
 		config  = new HikariConfig("jdbc/hikari.properties");
@@ -30,29 +31,18 @@ public class DatabaseConnect {
 		this.query = query;
 
 		try {
-
-			PreparedStatement pstmt = conn.prepareStatement(this.query);
-
-			// 3-2. 준비된 구문을 실행한다. 쿼리가 SELECT문인 경우 결과를 Set으로 받아온다.
-			ResultSet rs = pstmt.executeQuery();
-
-//			System.out.println("상품코드\t상품명\t제조일\t폐기일\t수량\t가격\n");
-//			while (rs.next()) {
-//
-//				System.out.printf("%-15s\t%-10s\t%-10s\t%-10s\t%-10d\t%-10d\n",
-//						rs.getString("PRODUCT_ID"), rs.getString("PRODUCT_NAME"), rs.getDate("MANU_DATE"),
-//						rs.getDate("DIS_DATE"), rs.getInt("QUANTITY"), rs.getInt("PRICE"));
-//
-//			}
-//
-//			rs.close();
-//			pstmt.close();
-//			ds.close();
-//			conn.close();
+			pstmt = conn.prepareStatement(this.query);		
+			rs = pstmt.executeQuery();
 
 		} catch (SQLException e) {
 			System.err.println("연결에 실패했습니다.");
 			e.printStackTrace();
 		}
 	}
+
+	public ResultSet getRs() {
+		return rs;
+	}
+
+	
 }
--- src/product/ProductMain.java
+++ src/product/ProductMain.java
@@ -1,21 +1,30 @@
 package product;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 import db.DatabaseConnect;
 
 public class ProductMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException{
+		String query = "SELECT * FROM PRODUCT";
+		
+		DatabaseConnect db = new DatabaseConnect(query);	
+		ResultSet rs = db.getRs();
 
-		//DatabaseConnect db = new DatabaseConnect();
-		
-		
+		System.out.println("상품코드\t상품명\t제조일\t폐기일\t수량\t가격\n");
+		while (rs.next()) {
+			
+			System.out.printf("%-8s\t%-8s\t%-10s\t%-10s\t%-10d\t%-10d\n",
+			rs.getString("PRODUCT_ID"),
+			rs.getString("PRODUCT_NAME"),
+			rs.getString("MANU_DATE"),
+			rs.getString("DIS_DATE"),
+			rs.getInt("QUANTITY"),
+			rs.getInt("PRICE"));
+		}
+				
+		}
 	}
-}
+		
+
