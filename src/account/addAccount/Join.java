b80d13441d252d80f78cd9860f3bed4df7a7e65b
Young <Young@DESKTOP-IT9UM9K>
2020-12-08 17:26:05 +0900

    res commit

--- src/account/addAccount/Join.java
+++ src/account/addAccount/Join.java
@@ -17,8 +17,9 @@ public class Join extends JFrame {
 	public JTextField[] infos = new JTextField[8]; 
 	ButtonGroup gb;
 	public JRadioButton rb1, rb2;
+	JTextField[] res_infos = new JTextField[2];
 
-	String [] info_names = {"ID","PW","NAME","RES_NO","PHONE","ADDRESS","SEX","MAIL_ADD"};
+	String [] info_names = {"아이디","패스워드","이름","주민번호","전화번호","주소","성별","메일주소"};
 	JLabel[] names = new JLabel[8];
 	JButton join = new JButton("JOIN");
 	
@@ -33,14 +34,20 @@ public class Join extends JFrame {
 		for (int i = 0; i<8; i++) {
 			infos[i] = new JTextField(10);
 			names[i] = new JLabel(info_names[i]);
-			if(i != 6) {
+			
+			if(i != 3 && i != 6) {
 				infos[i].setBounds(100, 20 + i*50,100,20);
 				add(infos[i]);
 			}
+			
 			names[i].setBounds(30, 20 + i*50,100,20);
 			add(names[i]);
 		}
 		
+		for (int i = 0; i < 2; i++) {
+			res_infos[i] = new JTextField(10);
+			res_infos[i].setBounds(100, 20 + i*50,100,20);
+		}
 		rb1.setBounds(95, 320,50,20);
 	    rb2.setBounds(150, 320,50,20);
 	    add(rb1);
