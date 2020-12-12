52f7184bacde3c2dffae6a79635787c917ac6ca4
Young <Young@DESKTOP-IT9UM9K>
2020-12-12 23:03:09 +0900

    popup

--- src/stocksub/stockAction/Disposal_Action.java
+++ src/stocksub/stockAction/Disposal_Action.java
@@ -4,9 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import stocksub.StockDao;
-import stocksub.stockView.StockInfoView;
 import stocksub.stockframe.StockInfoFrame;
 
 public class Disposal_Action implements ActionListener{
@@ -22,18 +23,22 @@ public class Disposal_Action implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		if (stockInfoF.stockInfoView.stockTable.getSelectedRow() != -1) {
 			
+			agree =  JOptionPane.showConfirmDialog(null,"     정말 폐기하시겠습니까?","확인",JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
 
 			int row = stockInfoF.stockInfoView.stockTable.getSelectedRow();
 			String product_id = ((String) stockInfoF.stockInfoView.tblModel.getValueAt(row, 0));
 			String dis_date = ((String) stockInfoF.stockInfoView.tblModel.getValueAt(row, 4));
 			
-			
-			sdao.Disposal_product(product_id, dis_date);
-			stockInfoF.stockInfoView.tblModel.setNumRows(0);
-			stockInfoF.stockInfoView.addStockLine(sdao.stockInfos(stockInfoF.stockInfoView.product_id));
-			
-			stockInfoF.stoview.tblModel.setNumRows(0);
-			stockInfoF.stoview.addStockLine(sdao.stockAll());
+			if (agree == JOptionPane.YES_OPTION) {
+				sdao.Disposal_product(product_id, dis_date);
+				stockInfoF.stockInfoView.tblModel.setNumRows(0);
+				stockInfoF.stockInfoView.addStockLine(sdao.stockInfos(stockInfoF.stockInfoView.product_id));
+				
+				stockInfoF.stoview.tblModel.setNumRows(0);
+				stockInfoF.stoview.addStockLine(sdao.stockAll());
+			} else {
+				
+			}
 			
 			
 
