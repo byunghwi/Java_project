6fa0cdefdcbad7a04e55072d747d2c24b34100cf
Young <Young@DESKTOP-IT9UM9K>
2020-12-21 17:55:54 +0900

    dzdz

--- src/account/addAccount/Join.java
+++ src/account/addAccount/Join.java
@@ -7,6 +7,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
@@ -23,12 +24,15 @@ public class Join extends JFrame {
 	public JTextField[] phone_infos = new JTextField[3];
 	JLabel minus[] = {new JLabel("-"),new JLabel("-"),new JLabel("-")};
 	ImageIcon join_Action_Button = new ImageIcon("src/회원가입버튼.jpg");
+	JPanel pIn;
+	JLabel joinText = new JLabel("회원 가입");
 
 	String [] info_names = {"아이디","비밀번호","이름","주민등록번호","전화번호","주소","성별","메일주소"};
 	JLabel[] names = new JLabel[8];
 	RoundedButton join = new RoundedButton("회 원 가 입",68,68,68);
 	
 	Font f1 = new Font("나눔고딕", Font.BOLD, 13);
+	Font f2 = new Font("나눔고딕", Font.BOLD, 30);
 
 	public Join() {
 	
@@ -36,51 +40,61 @@ public class Join extends JFrame {
 	     rb2 = new JRadioButton("F");
 	     gb = new ButtonGroup();
 	     gb.add(rb1);
-		 gb.add(rb2);
-	    
+	     gb.add(rb2);
+		 
+	    joinText.setFont(f2);
+	    joinText.setForeground(new Color(255, 255, 255));
+	 	pIn = new JPanel();
+		pIn.setBackground(new Color(68, 68, 68));
+		pIn.setBounds(0, 0, 200, 550);
+		add(pIn);
+		pIn.add(joinText).setBounds(100, 100, 100, 130);
+		pIn.setFocusable(false);
+		 
 		for (int i = 0; i<8; i++) {
 			infos[i] = new JTextField(10);
 			names[i] = new JLabel(info_names[i]);
 			
 			if(i != 3 && i != 4 && i != 6) {
-				infos[i].setBounds(150, 35 + i*55,130,20);
+				infos[i].setBounds(350, 35 + i*55,130,20);
 				add(infos[i]);
 			}
 			
 			names[i].setFont(f1);
 			names[i].setForeground(Color.gray);
-			names[i].setBounds(30, 35 + i*55,100,20);
+			names[i].setBounds(230, 35 + i*55,100,20);
 			add(names[i]);
 		}
 		
 		for (int i = 0; i < 2; i++) {
 			res_infos[i] = new JTextField(10);
-			res_infos[i].setBounds(150 + i*70, 200,60,20);
+			res_infos[i].setBounds(350 + i*70, 200,60,20);
 			add(res_infos[i]);
 		}
-		minus[0].setBounds(212, 205, 5, 5);
+		minus[0].setBounds(412, 205, 5, 5);
 		add(minus[0]);
 		
 		for (int i = 0; i < 3; i++) {
 			phone_infos[i] = new JTextField(10);
-			phone_infos[i].setBounds(150 + i*46, 255,40,20);
+			phone_infos[i].setBounds(350 + i*46, 255,40,20);
 			add(phone_infos[i]);
 		}
 		
-		minus[1].setBounds(190, 260, 5, 5);
+		minus[1].setBounds(390, 260, 5, 5);
 		add(minus[1]);
 		
-		minus[2].setBounds(236, 260, 5, 5);
+		minus[2].setBounds(436, 260, 5, 5);
 		add(minus[2]);
 	
-		rb1.setBounds(150, 365,50,20);
-	    rb2.setBounds(215, 365,50,20);
+		rb1.setBounds(350, 365,50,20);
+	    rb2.setBounds(415, 365,50,20);
 	    rb1.setBackground(Color.WHITE);
 	    rb2.setBackground(Color.WHITE);
 	    add(rb1);
 	    add(rb2);
 		
-		join.setBounds(130, 466, 90, 30);
+	    join.setFont(f1);
+		join.setBounds(330, 466, 90, 30);
 		join.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		add(join);
 		
@@ -93,7 +107,7 @@ public class Join extends JFrame {
 		setLayout(null);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLocation(1050,300);
-		setSize(350, 550);
+		setSize(550, 550);
 		setVisible(true);
 	
 	}
