fd02fa579fa7d9d91f67541a65b5f045b5c4797b
jjw34 <jjw34@DESKTOP-B2R3338>
2020-12-21 09:16:25 +0900

    같은 날짜에 정산버튼 중복 방지

--- src/calc/BtnAction/CalcBtnAction.java
+++ src/calc/BtnAction/CalcBtnAction.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 import calc.MainCalcFrame.CalcMainFrame;
 
 import calc.add.Calc_Add_Date;
+import calc.add.check_Calc;
 import main.MainFrame;
 
 
@@ -27,7 +28,7 @@ public class CalcBtnAction implements ActionListener{
 		Object ob = e.getSource();
 
 		if (ob == mainFrame.calcBtnPanel.calc_Btn) {
-			new Calc_Add_Date(mainFrame.mem_id);
+			new check_Calc(mainFrame.mem_id);
 			JOptionPane.showMessageDialog(null, "[SYSTEM] 금일정산처리되었습니다.", "확인", JOptionPane.CLOSED_OPTION);
 			mainFrame.calcBtnPanel.calc_Btn.setEnabled(false);
 		} 
