7c146cc99d10016e367bb3864d77a93723b0fedf
jjw34 <jjw34@DESKTOP-B2R3338>
2020-12-21 09:26:19 +0900

    정산버튼 중복 오류 해결

--- src/calc/BtnAction/CalcBtnAction.java
+++ src/calc/BtnAction/CalcBtnAction.java
@@ -30,7 +30,7 @@ public class CalcBtnAction implements ActionListener{
 		if (ob == mainFrame.calcBtnPanel.calc_Btn) {
 			new check_Calc(mainFrame.mem_id);
 			JOptionPane.showMessageDialog(null, "[SYSTEM] 금일정산처리되었습니다.", "확인", JOptionPane.CLOSED_OPTION);
-			mainFrame.calcBtnPanel.calc_Btn.setEnabled(false);
+			//mainFrame.calcBtnPanel.calc_Btn.setEnabled(false);
 		} 
 
 	}
