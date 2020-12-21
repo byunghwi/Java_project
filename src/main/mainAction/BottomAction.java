8a1444a1a45d0f2653ca293a488bb8a75406b873
82105 <82105@DESKTOP-7R346KH>
2020-12-21 15:33:43 +0900

    mainaction

--- src/main/mainAction/BottomAction.java
+++ src/main/mainAction/BottomAction.java
@@ -29,21 +29,14 @@ public class BottomAction implements ActionListener {
 
 		if (ob == mainFrame.bottomPanel.productBtn) {
 			// 메인 버튼 클릭시 색 변경해주기
-<<<<<<< HEAD
-			mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.productBtn);
-			mainFrame.cardlayout.show(mainFrame.centerView, "productView");
-=======
 			//mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.productBtn);
 			mainFrame.cardlayout.show(mainFrame.centerView, "productView");	// 가운데 화면 변경
 			mainFrame.btnlayout.show(mainFrame.pBtnView, "prodBtnPanel");	 // 우측 버튼 변경
 			
->>>>>>> branch 'develop' of https://github.com/byunghwi/Java_project.git
 		} else if (ob == mainFrame.bottomPanel.saleBtn) {
 			// 필드 초기화 먼저.
 			mainFrame.salePanel.prodnameTf.setText("");
 			mainFrame.salePanel.prodQt.setText("");
-//<<<<<<< HEAD
-//=======
 			
 			//재고화면 장바구니화면 초기화
 			mainFrame.salePanel.bucketTblModel.setNumRows(0);
@@ -52,16 +45,10 @@ public class BottomAction implements ActionListener {
 			//재고화면 로우 새로 받아오기.
 			mainFrame.salePanel.addStockLine(mainFrame.stockdao.stockAll());
 			
-//>>>>>>> branch 'develop' of https://github.com/byunghwi/Java_project.git
 			// 메인 버튼 클릭시 색 변경해주기
-<<<<<<< HEAD
-			mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.saleBtn);
-			mainFrame.cardlayout.show(mainFrame.centerView, "salePanel");
-=======
 			//mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.saleBtn);
 			mainFrame.cardlayout.show(mainFrame.centerView, "salePanel"); // 가운데 화면 변경
 			mainFrame.btnlayout.show(mainFrame.pBtnView, "saleBtnPanel"); // 우측 버튼 변경		
->>>>>>> branch 'develop' of https://github.com/byunghwi/Java_project.git
 
 			// 판매 패널의 J테이블에서 로우 선택시 발생하는 이벤트
 			mainFrame.salePanel.stockTable.addMouseListener(new MouseAdapter() {
@@ -79,12 +66,7 @@ public class BottomAction implements ActionListener {
 
 		}else if (ob == mainFrame.bottomPanel.stockBtn) {
 			//메인 버튼 클릭시 색 변경해주기
-<<<<<<< HEAD
-			System.out.println("재고버튼트느틑");
-			mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.stockBtn);
-=======
 			//mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.stockBtn);
->>>>>>> branch 'develop' of https://github.com/byunghwi/Java_project.git
 			mainFrame.cardlayout.show(mainFrame.centerView, "stockPanel");
 			mainFrame.btnlayout.show(mainFrame.pBtnView, "stockBtnPanel");
 			mainFrame.stockPanel.tblModel.setNumRows(0);
@@ -105,14 +87,9 @@ public class BottomAction implements ActionListener {
 			mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.commuteBtn);
 		}else if (ob == mainFrame.bottomPanel.eventBtn) {
 			//메인 버튼 클릭시 색 변경해주기
-<<<<<<< HEAD
-			mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.eventBtn);
-			mainFrame.cardlayout.show(mainFrame.centerView, "eventPanel");	
-=======
 			//mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.eventBtn);
 			mainFrame.cardlayout.show(mainFrame.centerView, "eventPanel");	// 가운데 화면 변경
 			mainFrame.btnlayout.show(mainFrame.pBtnView, "eventBtnPanel");	 // 우측 버튼 변경
->>>>>>> branch 'develop' of https://github.com/byunghwi/Java_project.git
 		}else if (ob == mainFrame.bottomPanel.calcBtn) {
 			//메인 버튼 클릭시 색 변경해주기
 			//mainFrame.bottomPanel.selectedBtn(mainFrame.bottomPanel.calcBtn);
