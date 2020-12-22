f5fbd322aca266b78cd51e32bc7b6dbc3fd58a89
Young <Young@DESKTOP-IT9UM9K>
2020-12-22 10:22:31 +0900

    id

--- src/account/findAccount/FindIdFrame.java
+++ src/account/findAccount/FindIdFrame.java
@@ -5,10 +5,10 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import common.RoundedButton;
@@ -20,23 +20,38 @@ public class FindIdFrame extends JFrame {
 	
 	public FindIdFrame() {
 		
+		Font f1 = new Font("나눔고딕", Font.BOLD, 15);
 		JLabel name = new JLabel("이름");
 		JLabel phone_number = new JLabel("전화번호");
 		JTextField name_text = new JTextField(10);
 		JTextField phone_text = new JTextField(10);
+		JPanel pIn;
+		JLabel findIDText = new JLabel("ID 찾기");
+		findIDText.setFont(f1);
+		
+		findIDText.setForeground(new Color(255, 255, 255));
+		pIn = new JPanel();
+		pIn.setBackground(new Color(68, 68, 68));
+		pIn.setBounds(0, 0, 80, 550);
+		add(pIn);
+		pIn.add(findIDText).setBounds(17, 5, 150, 40);
+		
+		
+		pIn.setLayout(null);
+		pIn.setFocusable(false);
 		
 		name.setFont(new Font("나눔고딕", Font.BOLD, 15));
 		phone_number.setFont(new Font("나눔고딕", Font.BOLD, 15));
 		
-		add(name).setBounds(15, 25, 40, 20);
-		add(phone_number).setBounds(15, 70, 100, 20);
+		add(name).setBounds(90, 25, 40, 20);
+		add(phone_number).setBounds(90, 70, 100, 20);
 		
-		add(name_text).setBounds(100, 25, 100, 20);
-		add(phone_text).setBounds(100, 70, 100, 20);
+		add(name_text).setBounds(175, 25, 100, 20);
+		add(phone_text).setBounds(175, 70, 100, 20);
 		
 		
 		find.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		add(find).setBounds(107, 110, 90, 30);
+		add(find).setBounds(182, 110, 90, 30);
 		find.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		
 		JFrame j1 = this;
@@ -61,7 +76,7 @@ public class FindIdFrame extends JFrame {
 		this.getContentPane().setBackground(Color.WHITE);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLocation(600,400);
-		setSize(240, 200);
+		setSize(320, 200);
 		setVisible(true);
 	}
 
